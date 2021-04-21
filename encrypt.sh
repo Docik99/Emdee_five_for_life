3928334ce310829fa517ac4155d0c305b27962b5
Docik99 <32065929+Docik99@users.noreply.github.com>
2021-04-21 22:04:11 +0300

    save cookie

--- encrypt.sh
+++ encrypt.sh
@@ -2,7 +2,6 @@
 
 server=$1
 
-hash=$(curl -s ${server} | head -n 6 | tail -n1 | cut -d '>' -f4 | cut -d '<' -f1 | md5sum | cut -d ' ' -f1)
+hash=$(curl -c 'cookie.txt' -s $server | head -n 6 | tail -n1 | cut -d '>' -f4 | cut -d '<' -f1 | tr -d '\n' | md5sum | cut -d ' ' -f1)
 
-curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' --data "hash=$hash" $server
-
+curl -s -X POST -b 'cookie.txt' -H 'Content-Type: application/x-www-form-urlencoded' --data "hash=$hash" $server
